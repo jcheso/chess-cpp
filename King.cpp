08537bd410a83260b3d1e1232ee380fa85c54cdd
Jarryd Cheso <jarryd.cheso@gmail.com>
2021-12-05 10:02:46 +0000

    fix: move abs on king logic

--- King.cpp
+++ King.cpp
@@ -7,7 +7,7 @@
 King::King(int colour, std::string name, int rank, int file) : ChessPiece(colour, name, rank, file) {}
 bool King::isLegalMove(int toRank, int toFile, ChessBoard *cb) {
     // Check the path is clear and it is a magnitude of 1 - direction does not matter for King
-    if (isPathClear(toRank, toFile, cb) && ((currentFile - toFile) == abs(1) || (currentRank - toRank) == abs(1)))
+    if (isPathClear(toRank, toFile, cb) && ((abs(currentFile - toFile) == 1) || (abs(currentRank - toRank) == 1)))
         return true;
     else
         return false;
